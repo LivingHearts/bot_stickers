a02d79a8c504abe421f0b800f0faba10e77fb170
Valerii <valerii@MacBook-Pro-Vova.local>
2025-04-24 10:04:10 +0300

    bot

---
+++ dist/config.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.STICKER_SET_NAME = exports.ALLOWED_USERS = exports.ALLOWED_GROUPS = exports.TOKEN = void 0;
+exports.TOKEN = "7811456678:AAFkgfsPfTgCUftTV_pYcS1lb3L7_31mEk8";
+// Перелік ID груп, де бот може працювати
+exports.ALLOWED_GROUPS = [-1002641763276, 7811456678, 846282286, 1250458483];
+// Список користувачів, які мають доступ
+exports.ALLOWED_USERS = [846282286, 7811456678, 1250458483];
+// Строка для доступу до стікерів
+exports.STICKER_SET_NAME = "living_heart";
