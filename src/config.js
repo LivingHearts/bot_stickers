71b8a113e1f00da5ac8159457fd2ad4d6076e44f
Valerii <valerii@MacBook-Pro-Vova.local>
2025-04-24 23:38:56 +0300

    bot

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@
 export const TOKEN = "7980095567:AAFZRHKqYa53UY_gAId7VNit-PFhMZZ1Adc"; 
 
 // ✅ ID дозволених груп
-export const ALLOWED_GROUPS = [-1002641763276, 7811456678, 846282286, 1250458483];
+export const ALLOWED_GROUPS = [-1002641763276, 7811456678, 846282286];
 
 // ✅ ID дозволених користувачів
-export const ALLOWED_USERS = [846282286, 7811456678, 1250458483];
+export const ALLOWED_USERS = [846282286, 7811456678];
 
 // 🎨 Назва набору стікерів
 export const STICKER_SET_NAME = "living_heart";
